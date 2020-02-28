85ed7a7663574af66d1c62b95511c70c121652a5
SubTachyon <SubTachyon@users.noreply.github.com>
2020-02-28 13:26:41 +0100

    Basic hiearchical login

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ import firebase, { auth, provider } from './Database/Firebase.js';
 class App extends Component {
   constructor() {
     super();
+    
+    this.constants = {
+      listOfAdmins: [
+        "petr.kudlacek@broadcom.com",
+        "subcryptus@gmail.com"
+      ],
+      listOfShops: [
+        "subtachyon@gmail.com",
+        "sublepton@gmail.com"
+      ]
+    }
+
     this.state = {
       loggedInUserInfo: {
         email: '',
         name: '',
         photoURL: ''
-      },
-      items: [],
+      },      
       user: null
     }
 
@@ -25,23 +36,52 @@ class App extends Component {
         this.setState({
           user
         });
+        //Pull out relevant info into our state object
         this.setState({
           loggedInUserInfo: {
             email: user.email,
             name: user.displayName,            
             photoURL: user.photoURL
           }
-        });
+        });      
       });      
+  }
+
+  isAdmin(email)
+  {
+    if (this.constants.listOfAdmins.includes(email)) {
+      return true;
+    }
+    return false;
+  }
+
+  isShop(email)
+  {
+    if (this.constants.listOfShops.includes(email)) {
+      return true;
+    }
+    return false;
   }
   
   render() {
     return (
       <div className='app'>
+        {!this.state.user ?
         <button onClick={this.login}>Log In</button>   
+         : 
         <div>
           <p>Username: {this.state.loggedInUserInfo.name}</p>
           <p>Email: {this.state.loggedInUserInfo.email}</p>
+          <img src={this.state.loggedInUserInfo.photoURL} />
+        </div>
+        }
+        <div>
+          <p>Your role is:</p>
+          {this.isAdmin(this.state.loggedInUserInfo.email) ?
+          <p>Admin!</p>
+          :
+          <p>pleb...</p>
+          }
         </div>
       </div>  
     );
